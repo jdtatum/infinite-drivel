e4a192c5268ceab2fa2542f4e8e6c7bb6a1b7239
Darek Tatum <jdtatum@gmail.com>
2017-12-06 21:49:38 -0600

    Infinite Drivel begins

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBawjuTwytEYP0MxdoSoo61ZkBW0bAPx2g",
+  authDomain: "infinite-drivel.firebaseapp.com",
+  databaseURL: "https://infinite-drivel.firebaseio.com",
+});
+
+export default base;
