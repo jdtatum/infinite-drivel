58a0bd971127949b93495dcc4c5ef398250a2d05
Darek Tatum <jdtatum@gmail.com>
2017-12-06 22:18:43 -0600

    Login component created

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import Login from './Login';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.renderLogin = this.renderLogin.bind(this);
+    this.state = {
+      uid: null
+    }
+  }
+
+  renderLogin() {
+    if(!this.state.uid) {
+      return (
+        <Login />
+      )
+    }
+  }
+
   render() {
+    if(!this.state.uid) {
+      return <div>{this.renderLogin()}</div>
+    }
     return (
       <div className="app">
-
+        <h2>App</h2>
       </div>
     );
   }
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div className="login">
+        <h2>Login</h2>
+      </div>
+    );
+  }
+}
+
+export default Login;
